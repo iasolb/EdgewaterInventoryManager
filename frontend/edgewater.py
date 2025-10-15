7dbd52e11a2c9c5ed393469d5569e710bfe2ed94
iasolb <ianspraguesolberg@gmail.com>
2025-10-15 15:42:20 -0400

    added images to data folder for assets

--- frontend/edgewater.py
+++ frontend/edgewater.py
@@ -1,5 +1,23 @@
 import streamlit as st
-from rest.api import EdgewaterAPI
+from api import EdgewaterAPI
+from database import Database
 
-
+db = Database()
 api = EdgewaterAPI()
+
+import streamlit as st
+
+page_bg_img = """
+    <style>
+    .stApp {
+        background-image: url("YOUR_IMAGE_URL_HERE");
+        background-size: cover;
+        background-attachment: fixed; /* Optional: keeps image fixed while scrolling */
+    }
+    </style>
+    """
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# Your Streamlit app content goes here
+st.title("My Streamlit App with Background Image")
+st.write("This is some content on top of the background.")
--- rest/api.py
+++ rest/api.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 import FastAPI
+from database import Database
 from typing import List, Dict, Any
 from loguru import logger
 
@@ -8,5 +9,5 @@ from loguru import logger
 class EdgewaterAPI:
     """Class to interact with Edgewater API"""
 
-    def __call__(self, *args, **kwds):
+    def __init__(self, *args, **kwds):
         pass
